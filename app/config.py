2636e9f02d528fa164d57aabcf003921ef13823c
DDR-1 <73418471+DDR-1@users.noreply.github.com>
2024-09-02 00:07:08 +0530

    adding project files

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://postgres:Devesh12@localhost:5432/storedb')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'postgresql://username:password@localhost:5432/storedb')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
